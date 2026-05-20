ceab39529a1729ead9ed77367f5f26b5f64ab2d0
nickolas <nicko.vitor@gmail.com>
2026-05-19 21:57:07 -0300

    Fase 4 - Etapa 4: Regra 2 - deteccao de codigo morto
    Cria AvaliadorConstante: avalia expressoes booleanas em tempo de
    compilacao por instanceof dispatch sobre ValorBooleano, ExpNot, ExpAnd
    e ExpOr. Retorna null para qualquer sub-expressao nao-constante,
    evitando falsos positivos.
    
    Atualiza VisitorLinter com traversal completo da AST (visitarComando e
    visitarDeclaracao, incluindo corpos de procedimentos) e implementa a
    Regra 2: if com condicao sempre true emite CODIGO_MORTO_RAMO_ELSE, if
    com condicao sempre false emite CODIGO_MORTO_RAMO_THEN, while com
    condicao sempre false emite CODIGO_MORTO_WHILE. Ramos continuam sendo
    visitados apos o aviso para que a Regra 3 (Etapa 5) contabilize
    complexidade corretamente.

--- PLP_2026/Imperativa2/src/li2/plp/imperative2/linter/VisitorLinter.java
+++ PLP_2026/Imperativa2/src/li2/plp/imperative2/linter/VisitorLinter.java
@@ -4,7 +4,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import li2.plp.expressions2.expression.Expressao;
+import li2.plp.imperative1.command.Comando;
+import li2.plp.imperative1.command.ComandoDeclaracao;
+import li2.plp.imperative1.command.IfThenElse;
+import li2.plp.imperative1.command.SequenciaComando;
+import li2.plp.imperative1.command.While;
+import li2.plp.imperative1.declaration.Declaracao;
+import li2.plp.imperative1.declaration.DeclaracaoComposta;
 import li2.plp.imperative2.Programa;
+import li2.plp.imperative2.declaration.DeclaracaoProcedimento;
 import li2.plp.imperative2.semantica.Escopo;
 import li2.plp.imperative2.semantica.Simbolo;
 import li2.plp.imperative2.semantica.SimboloKind;
@@ -13,6 +22,7 @@ import li2.plp.imperative2.semantica.TabelaSimbolos;
 public class VisitorLinter {
 
     private final TabelaSimbolos tabela;
+    private final AvaliadorConstante avaliador = new AvaliadorConstante();
     private final List<AvisoLinter> avisos = new ArrayList<>();
 
     public VisitorLinter(TabelaSimbolos tabela) {
@@ -21,8 +31,13 @@ public class VisitorLinter {
 
     public List<AvisoLinter> analisar(Programa programa) {
         verificarVariaveisNaoUtilizadas();
+        if (programa != null && programa.getComando() != null) {
+            visitarComando(programa.getComando());
+        }
         return Collections.unmodifiableList(avisos);
     }
+
+    // ---- Regra 1: variavel nao utilizada ----
 
     private void verificarVariaveisNaoUtilizadas() {
         for (Escopo escopo : tabela.getTodosEscopos()) {
@@ -38,5 +53,58 @@ public class VisitorLinter {
                 }
             }
         }
+    }
+
+    // ---- Traversal da AST ----
+
+    private void visitarComando(Comando c) {
+        if (c instanceof SequenciaComando) {
+            SequenciaComando s = (SequenciaComando) c;
+            visitarComando(s.getComando1());
+            visitarComando(s.getComando2());
+        } else if (c instanceof ComandoDeclaracao) {
+            ComandoDeclaracao cd = (ComandoDeclaracao) c;
+            visitarDeclaracao(cd.getDeclaracao());
+            visitarComando(cd.getComando());
+        } else if (c instanceof IfThenElse) {
+            visitarIfThenElse((IfThenElse) c);
+        } else if (c instanceof While) {
+            visitarWhile((While) c);
+        }
+    }
+
+    private void visitarDeclaracao(Declaracao d) {
+        if (d instanceof DeclaracaoComposta) {
+            DeclaracaoComposta dc = (DeclaracaoComposta) d;
+            visitarDeclaracao(dc.getDeclaracao1());
+            visitarDeclaracao(dc.getDeclaracao2());
+        } else if (d instanceof DeclaracaoProcedimento) {
+            visitarComando(((DeclaracaoProcedimento) d).getDefProcedimento().getComando());
+        }
+    }
+
+    // ---- Regra 2: codigo morto ----
+
+    private void visitarIfThenElse(IfThenElse c) {
+        Expressao cond = c.getExpressao();
+        Boolean valor = avaliador.avaliar(cond);
+        if (Boolean.TRUE.equals(valor)) {
+            avisos.add(new AvisoLinter(AvisoCodigo.CODIGO_MORTO_RAMO_ELSE,
+                    "ramo 'else' inalcancavel porque a condicao e sempre 'true'"));
+        } else if (Boolean.FALSE.equals(valor)) {
+            avisos.add(new AvisoLinter(AvisoCodigo.CODIGO_MORTO_RAMO_THEN,
+                    "ramo 'then' inalcancavel porque a condicao e sempre 'false'"));
+        }
+        visitarComando(c.getComandoThen());
+        visitarComando(c.getComandoElse());
+    }
+
+    private void visitarWhile(While c) {
+        Boolean valor = avaliador.avaliar(c.getExpressao());
+        if (Boolean.FALSE.equals(valor)) {
+            avisos.add(new AvisoLinter(AvisoCodigo.CODIGO_MORTO_WHILE,
+                    "corpo do 'while' inalcancavel porque a condicao e sempre 'false'"));
+        }
+        visitarComando(c.getComando());
     }
 }
